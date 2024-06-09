e1d90cf37dd28396a2c039eb911833eb867cf6f3
Alan Rodriguez <gheryk90@gmail.com>
2024-06-09 19:54:25 +0100

    refactoring components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -8,7 +8,6 @@ import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
 const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-console.log(storedIds)
 const sortedPlaces = storedIds.map((id) => 
   AVAILABLE_PLACES.find((place) => place.id === id )
 );
@@ -51,7 +50,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -59,7 +58,7 @@ function App() {
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
-  }
+  } ,[])
 
   return (
     <>
